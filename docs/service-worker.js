9faad6b08bb2e45e086aac215aad0db5e585d127
Daniel Jackson <email@danieljackson.co.uk>
2020-12-15 15:01:25 +0000

    Build

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -58,4 +58,4 @@ self.addEventListener('activate', function (event) {
   );
 });
 
-const cacheName = 'v' /* STRING TO BE COMPLETED AT BUILD TIME */ + '20200729204043190';
+const cacheName = 'v' /* STRING TO BE COMPLETED AT BUILD TIME */ + '20201215150115594';
