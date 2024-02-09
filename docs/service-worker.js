7be1f3a9f4a576f510a023865da9468910094526
Daniel Jackson <email@danieljackson.co.uk>
2024-02-09 14:09:17 +0000

    Auto-version index page, release.

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -58,4 +58,4 @@ self.addEventListener('activate', function (event) {
   );
 });
 
-const cacheName = 'v' /* STRING TO BE COMPLETED AT BUILD TIME */ + '20240126155007962';
+const cacheName = 'v' /* STRING TO BE COMPLETED AT BUILD TIME */ + '20240209140824584';
